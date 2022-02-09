15b0ed28d866105b8c4ddeca4287709a9c0741bc
Semm6 <sem.dewilde6@gmail.com>
2022-02-09 13:08:08 +0100

    Added navigation with components

--- Soundify/Pages/Home.js
+++ Soundify/Pages/Home.js
@@ -5,14 +5,20 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
       <StatusBar style="auto" />
+      <Text>Home</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  text: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
