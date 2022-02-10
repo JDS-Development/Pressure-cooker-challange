884ba61990230a9d91c618d56f71cf762706634f
Jovi Simons <jovisimons009@gmail.com>
2022-02-10 14:46:43 +0100

    chat

--- Soundify/Pages/PartyChat.js
+++ Soundify/Pages/PartyChat.js
@@ -1,16 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { TextInput } from 'react-native-paper';
+
 
 export default function PartyChat() {
 
+  const [text, setText] = useState("");
+
+  const [chat, setChat] = useState("");
+
+  const onEnter = () => {
+    setChat([...chat, text])
+  }
+
+
   return (
     <View style={styles.container}>
-        <View style={{ flex: 1 }} >
-            <Text>Party chat!</Text>
-        </View>
+        <View style={styles.chatContainer}>
+          <FlatList
+                  data={chat}
+                  renderItem={({item}) => <Text>{item}</Text>}
+            />
+          </View>
 
-      
+        <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'flex-end' }}>
+          <TextInput
+            label="Chat here..."
+            value={text}
+            onChangeText={text => setText(text)}
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                onEnter()
+              }
+            }}
+          />
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +45,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+      },
+      chatContainer: {
+        flex: 5,
+        alignItems: 'flex-end'
       },
 });
